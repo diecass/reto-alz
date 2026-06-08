4ed1c991234df19aa025ff8a3dd06b02f86684e8
diecass <141677264+diecass@users.noreply.github.com>
2026-06-08 08:08:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.decomposition import PCA
 
+st.write("Archivos encontrados:")
+
+for f in DEFAULT_MODELS_DIR.glob("*"):
+    st.write(f.name)
+
+
 st.set_page_config(
     page_title="Alzheimer ML App",
     page_icon="🧠",
